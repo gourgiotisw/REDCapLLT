82051a6fd1f832e84ff356d738a199b3dbc61556
William Gourgiotis <64996193+gourgiotisw@users.noreply.github.com>
2024-07-22 17:12:00 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="A package to translate REDCap language libraries",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/REDCapLLT",
+    url="https://github.com/gourgiotisw/REDCapLLT",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
